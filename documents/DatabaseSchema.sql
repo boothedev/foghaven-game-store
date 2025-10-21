8a3d463eb51487cd0f13df21496751d5ebd8e760
Quang Pham <quangpham@ucmerced.edu>
2025-10-20 19:57:49 -0700

    add phase 1 database schema sql queries

---
+++ documents/DatabaseSchema.sql
@@ -0,0 +1,104 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    balance INTEGER NOT NULL DEFAULT 0,
+    is_active BOOLEAN NOT NULL DEFAULT 1,
+    is_public BOOLEAN NOT NULL DEFAULT 1,
+    is_admin BOOLEAN NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS games (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    price INTEGER,
+    introduction TEXT,
+    description TEXT,
+    developer TEXT,
+    publisher TEXT,
+    portrait TEXT,
+    landscape TEXT,
+    release_date DATETIME,
+    stars INTEGER DEFAULT 0,
+    raters INTEGER DEFAULT 0,
+    ratings REAL DEFAULT 0.0,
+    owners INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    icon TEXT,
+    weight INTEGER DEFAULT 100
+);
+
+CREATE TABLE IF NOT EXISTS platforms (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    value TEXT,
+    weight INTEGER NOT NULL DEFAULT 100
+);
+
+CREATE TABLE IF NOT EXISTS screenshots (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER NOT NULL,
+    thumbnail TEXT NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER NOT NULL,
+    thumbnail TEXT NOT NULL,
+    content_sd TEXT NOT NULL,
+    content_max TEXT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS achievements (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    thumbnail TEXT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS payment_cards(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    number TEXT NOT NULL,
+    exp_month INTEGER NOT NULL,
+    exp_year INTEGER NOT NULL,
+    security_code INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS game_users (
+    game_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    stars INTEGER,
+    owned_at DATETIME NOT NULL,
+    PRIMARY KEY (game_id, user_id),
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS game_genres (
+    game_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+    weight INTEGER DEFAULT 0,
+    PRIMARY KEY (game_id, genre_id),
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS game_platforms (
+    game_id INTEGER NOT NULL,
+    platform_id INTEGER NOT NULL,
+    min_requirements TEXT,
+    rec_requirements TEXT,
+    PRIMARY KEY (game_id, platform_id),
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+    FOREIGN KEY (platform_id) REFERENCES platforms(id) ON DELETE CASCADE
+);
